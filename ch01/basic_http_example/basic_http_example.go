fdd86dcfd319d40ab7a52c28803d09f8136cef7e
thisisnew <thisisnew@naver.com>
2021-08-10 22:18:24 +0900

    1장 마이크로서비스 소개

--- ch01/basic_http_example/basic_http_example.go
+++ ch01/basic_http_example/basic_http_example.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
-
-type helloWorldResponse struct {
-	Message string `json:"message"`
-}
 
 func main() {
 	port := 8080
 
 	http.HandleFunc("/helloworld", helloWorldHandler)
 
-	log.Printf("Server starting on port %v\n", port)
+	log.Printf("Server starting on port %v/n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	response := helloWorldResponse{Message: "HelloWorld"}
-
-	encoder := json.NewEncoder(w)
-	encoder.Encode(response)
+	fmt.Fprint(w, "Hello World")
+	return
 }
